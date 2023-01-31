b263cdfab802c7d1aee3349b7c04b1e2aca21e00
MichaelI.Insberg <michael.insberg@4micro.de>
2023-01-31 16:58:21 +0100

    Use asnc calls

--- Application/Program.cs
+++ Application/Program.cs
@@ -3,17 +3,38 @@
 using ServiceReference1;
 
 
+var toolbox = new ToolboxServiceClient();
 try
 {
-    var toolbox = new ToolboxServiceClient();
-    toolbox.Open();
-    var machinesRequest = new getMachinesRequest();
-    var machine = toolbox.getMachines(machinesRequest);
-    Console.WriteLine(machine);
+
+    try
+    {
+        await toolbox.OpenAsync();
+        Console.WriteLine($"State is: {toolbox.State}");
+        var machinesRequest = new getMachinesRequest();
+        Console.WriteLine(machinesRequest);
+
+        var machinesResponse = await toolbox.getMachinesAsync(machinesRequest);
+
+        var machines = machinesResponse.@return;
+        Console.WriteLine($"{machines.Length} machines received");
+
+        foreach (var machine in machines)
+        {
+            Console.WriteLine($"Machine: Product: {machine.Product} | MachineNumber {machine.MachineNumber}");
+        }
+    }
+    catch (System.Exception e)
+    {
+        Console.WriteLine($"Exception: {e.Message}");
+    }
+
 }
-catch (System.Exception e)
+finally
 {
-    Console.WriteLine(e);
+    
+    await toolbox.CloseAsync();
+    Console.WriteLine($"State is: {toolbox.State}");
 }
 
 Console.ReadLine();
