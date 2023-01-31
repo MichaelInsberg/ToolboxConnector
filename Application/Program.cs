a221744391e5db3e0d0e2ecd2c536d212733480f
MichaelI.Insberg <michael.insberg@4micro.de>
2023-01-31 16:33:05 +0100

    Initial push

---
+++ Application/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ServiceReference1;
+
+
+try
+{
+    var toolbox = new ToolboxServiceClient();
+    toolbox.Open();
+    var machinesRequest = new getMachinesRequest();
+    var machine = toolbox.getMachines(machinesRequest);
+    Console.WriteLine(machine);
+}
+catch (System.Exception e)
+{
+    Console.WriteLine(e);
+}
+
+Console.ReadLine();
+
+
